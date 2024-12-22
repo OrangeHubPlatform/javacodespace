76181eac62c6278cb46023aba7057875c6aa08ea
thiyanes97 <thiyaneswaran1997.mech@gmail.com>
2024-12-22 18:52:53 +0530

    [Dev] Spring Starter Project generated with Spring Initializer Java 21 and Spring Parent 3.4.1

---
+++ src/main/java/com/orangehub/springbootstarterapp/SpringbootstarterappApplication.java
@@ -0,0 +1,13 @@
+package com.orangehub.springbootstarterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootstarterappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootstarterappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orangehub/springbootstarterapp/SpringbootstarterappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orangehub.springbootstarterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootstarterappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
