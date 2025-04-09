4bccef0ade4c0d770bda5d1794a003af5b695614
thiyaneswaranvp <thiyaneswaranvp@gmail.com>
2025-04-09 07:36:12 +0000

    [DEV] Primary Java Run Files

---
+++ src/main/java/com/orangehub/javacodespace/JavacodespaceApplication.java
@@ -0,0 +1,13 @@
+package com.orangehub.javacodespace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacodespaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavacodespaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orangehub/javacodespace/JavacodespaceTests.java
@@ -0,0 +1,13 @@
+package com.orangehub.javacodespace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacodespaceTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
